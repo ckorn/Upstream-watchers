d1e5390c2b183e903a5ad837e6b3b2f295b9c1e2
Christoph Korn <c_korn@gmx.de>
2014-08-16 00:24:50 +0200

    Remove jag.py
    Upstream site was down for weeks now.

--- update.sh
+++ update.sh
@@ -3,7 +3,6 @@ python frostwire.py > frostwire.html 2> frostwire.log
 python warsow.py > warsow.html 2> warsow.log
 python snowstorm.py > snowstorm.html 2> snowstorm.log
 python wakeonplan.py > wakeonplan.html 2> wakeonplan.log
-python jag.py > jag.html 2> jag.log
 python jwltief.py > jwltief.html 2> jwltief.log
 python nikki.py > nikki.html 2> nikki.log
 python legends.py > legends.html 2> legends.log
