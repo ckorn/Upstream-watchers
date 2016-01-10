d408ba3eb2b9f2c9d642009ca6665580974c1f16
Christoph Korn <christoph.korn@posteo.de>
2016-01-10 20:05:33 +0100

    pycharm.py: Upstream uses JSON for release information
    Took me some JS debugging to get this URL :D

--- pycharm.py
+++ pycharm.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python
 import re
 import urllib2
+import json
 
 def main():
-	h=urllib2.urlopen("http://www.jetbrains.com/js2/version.js").read()
-	v_finder=re.compile('var versionPyCharmLong = "(?P<version>[\d\.]+)";', re.M)
-	m=v_finder.finditer(h)
-	if not m: return
-	for x in m:
-		v=x.group("version")
-		print "<a href='http://download-ln.jetbrains.com/python/pycharm-community-%(v)s.tar.gz'>%(v)s</a>"%locals()
+	h=urllib2.urlopen("https://data.services.jetbrains.com/products/releases?code=PCP%2CPCC&latest=true&type=release").read()
+	j=json.loads(h)
+	#print json.dumps(j, indent=4, sort_keys=True)
+	print j["PCC"][0]["downloads"]["linux"]["link"]
+	
 
 if __name__ == "__main__":
 	main()
